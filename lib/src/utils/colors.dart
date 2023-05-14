59b998e08e077444191bc9fd6b21dfdf2d5fe0b3
Emanuel Lamba <emanuellamba13@gmail.com>
2023-05-14 11:14:36 +0300

    feat: add custom sizes and colors for reusability purposes

--- lib/src/utils/colors.dart
+++ lib/src/utils/colors.dart
@@ -5,3 +5,5 @@ const Color primaryColor900 = Color(0xFFF0F0F0);
 const Color secondaryColor = Color(0xFF263144);
 const Color accentColor = Color(0xFFFFFFFF);
 const Color detailColor = Color(0xFF516DFF);
+const Color errorColor = Color.fromARGB(255, 240, 30, 30);
+const Color successColor = Color(0xFF5CB85C);
--- lib/src/utils/sizes.dart
+++ lib/src/utils/sizes.dart
@@ -1,15 +1,17 @@
 //Fonts
 const double smallFont = 12;
+const double snackBarFont = 15;
 const double defaultFont = 16;
 const double mediumFont = 22;
 const double bigFont = 40;
 
 //Delimiters
-const double defaultDelimiter = 30;
+const double defaultDelimiter = 16;
+const double formDelimiter = 32;
+// const double buttonSpaceDelimiter = 80;
 
 //Widget
 const double defaultSize = 16;
+const double zero = 0;
 
 //Device Sizes
-double? deviceWidth;
-double? deviceHeight;
